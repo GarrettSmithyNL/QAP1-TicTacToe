9beda4dca921d8e01bd55ae31c28cf1bbc3187ca
GarrettSmithyNL <garrett_smith_30@hotmail.com>
2024-10-06 18:08:53 -0230

    added ability to run test

--- src/test/java/com/keyin/MenuTest.java
+++ src/test/java/com/keyin/MenuTest.java
@@ -3,8 +3,6 @@ package com.keyin;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.io.ByteArrayInputStream;
 
 public class MenuTest {
--- src/test/java/com/keyin/PlayerTest.java
+++ src/test/java/com/keyin/PlayerTest.java
@@ -3,8 +3,6 @@ package com.keyin;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 public class PlayerTest {
 
   @Test
